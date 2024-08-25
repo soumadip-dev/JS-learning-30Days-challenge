d268650b3eae1034131254107d4f22607de8c14f
Soumadip Majila <soumadipmajila@gmail.com>
2024-08-25 14:46:13 +0530

    Update script.js

--- 27_project_task_management_app/script.js
+++ 27_project_task_management_app/script.js
@@ -6,35 +6,148 @@ function loadTasks() {
   return taskData;
 }
 
-function addTaskToLocalStorage(taskText) {
+function refreshTasks(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function addTaskToLocalStorage(task) {
   // Add a new task to localStorage
   const taskData = loadTasks();
-  taskData.tasklist.push(taskText);
+  taskData.tasklist.push({ ...task });
   localStorage.setItem("tasks", JSON.stringify(taskData));
 }
 
-function appendTaskToHtml(taskText) {
-  // Create and append a new task item to the HTML list
+function appendTaskToHtml(task) {
   const taskListElement = document.getElementById("taskList");
   const taskItem = document.createElement("li");
-  taskItem.textContent = taskText;
+
+  taskItem.setAttribute("data-id", task.id);
+  const taskContent = document.createElement("div");
+  taskContent.classList.add("task-content");
+
+  const taskTextElement = document.createElement("span");
+  if (task.isCompleted) {
+    taskTextElement.classList.add("completed");
+  }
+  taskTextElement.textContent = task.task;
+
+  const completeBtn = document.createElement("button");
+  completeBtn.classList.add("completeBtn");
+  completeBtn.textContent = task.isComplete ? "Reset" : "Completed";
+
+  completeBtn.addEventListener("click", toggleTask);
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("editBtn");
+  editBtn.textContent = "Edit";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("deleteBtn");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", deleteTask);
+
+  taskContent.appendChild(taskTextElement);
+  taskContent.appendChild(completeBtn);
+  taskContent.appendChild(editBtn);
+  taskContent.appendChild(deleteBtn);
+  taskItem.appendChild(taskContent);
+
   taskListElement.appendChild(taskItem);
+}
+
+function executeFilterAction(event) {
+  const taskListElement = document.getElementById("taskList");
+  const element = event.target;
+  const value = element.getAttribute("data-filter");
+  taskListElement.innerHTML = "";
+  const tasks = loadTasks();
+
+  if (value === "all") {
+    tasks.tasklist.forEach((task) => {
+      appendTaskToHtml(task);
+    });
+  } else if (value === "pending") {
+    tasks.tasklist.forEach((task) => {
+      if (task.isComplete !== true) {
+        appendTaskToHtml(task);
+      }
+    });
+  } else {
+    tasks.tasklist.forEach((task) => {
+      if (task.isComplete === true) {
+        appendTaskToHtml(task);
+      }
+    });
+  }
+}
+
+function toggleTask(event) {
+  const taskItem = event.target.parentElement.parentElement;
+  const taskId = taskItem.getAttribute("data-id");
+  const tasks = loadTasks();
+
+  tasks.tasklist.forEach((task) => {
+    if (task.id == taskId) {
+      task.isComplete = !task.isComplete;
+      const taskTextElement = taskItem.querySelector(".task-content span");
+      const completeBtn = taskItem.querySelector(".completeBtn");
+
+      if (task.isComplete) {
+        taskTextElement.classList.add("completed");
+        completeBtn.textContent = "Reset";
+      } else {
+        taskTextElement.classList.remove("completed");
+        completeBtn.textContent = "Completed";
+      }
+    }
+  });
+
+  refreshTasks(tasks);
+}
+
+function resetHtmlTasks(tasks) {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+  tasks.tasklist.forEach((task) => {
+    appendTaskToHtml(task);
+  });
+}
+
+function deleteTask(event) {
+  const taskItem = event.target.parentElement.parentElement;
+  const taskId = taskItem.getAttribute("data-id");
+  let tasks = loadTasks();
+  tasks.tasklist = tasks.tasklist.filter((task) => task.id != taskId);
+  refreshTasks(tasks);
+  resetHtmlTasks(tasks);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   // Initialize DOM elements
   const taskInputElement = document.getElementById("taskInput");
   const addButtonElement = document.getElementById("addTask");
+
+  let taskData = loadTasks();
+
   const taskListElement = document.getElementById("taskList");
+
+  const filterBtn = document.getElementsByClassName("filterButton");
+
+  for (const btn of filterBtn) {
+    btn.addEventListener("click", executeFilterAction);
+  }
 
   // Add task when button is clicked
   addButtonElement.addEventListener("click", () => {
-    const taskText = taskInputElement.value;
+    const taskText = taskInputElement.value.trim();
     if (taskText === "") {
       alert("Please write something for the task");
     } else {
-      addTaskToLocalStorage(taskText);
-      appendTaskToHtml(taskText);
+      taskData = loadTasks();
+      const id = taskData.tasklist.length;
+      const task = { task: taskText, isComplete: false, id };
+      addTaskToLocalStorage(task, id);
+      appendTaskToHtml(task, id);
       taskInputElement.value = "";
     }
   });
@@ -48,8 +161,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // Load and display existing tasks from localStorage
   const tasks = loadTasks();
   tasks.tasklist.forEach((task) => {
-    const newTaskItem = document.createElement("li");
-    newTaskItem.textContent = task;
-    taskListElement.appendChild(newTaskItem);
+    appendTaskToHtml(task);
   });
 });
