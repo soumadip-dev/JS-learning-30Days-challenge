8d7ece0cc50b6e65f652aa4fab57f868e14a1fc4
Soumadip Majila <soumadipmajila@gmail.com>
2024-11-01 12:33:35 +0530

    Update 18_algorithms.js

--- 18_algorithms.js
+++ 18_algorithms.js
@@ -7,7 +7,6 @@ function bubbleSort(arr) {
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
